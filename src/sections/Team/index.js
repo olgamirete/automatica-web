cc9852ae2955f0f370f4639b1560e8abe440d965
Federico Giancarelli <olgamirete@gmail.com>
2022-03-01 10:11:43 +0100

    updated team info

--- src/sections/Team/index.js
+++ src/sections/Team/index.js
@@ -13,11 +13,9 @@ function Team(props) {
         <InfoCard variant="dark" bgOpacity={6} className="border border-muted" >
             <h1 className="text-light mb-4 text-center">Nuestro equipo</h1>
             <Row xs={1} sm={2} className="d-flex justify-content-center">
-                <MemberCard picSrc={picFederico} name="Federico" subtitle="Project leader" urlLinkedIn="https://www.linkedin.com/in/fgiancarelli/" urlGitHub="https://github.com/olgamirete/" />
+                <MemberCard picSrc={picFederico} name="Federico" subtitle="Project leader" urlLinkedIn="https://www.linkedin.com/in/fgiancarelli/" urlGitHub="https://github.com/omirete/" />
                 <MemberCard picSrc={picJulieta} name="Julieta" subtitle="Marketing" urlLinkedIn="https://www.linkedin.com/in/julietamo/" />
                 <MemberCard picSrc={picFrancisco} name="Francisco" subtitle="Technical leader" urlLinkedIn="https://www.linkedin.com/in/giancarellifrancisco/" urlGitHub="https://github.com/padapada09/" />
-                <MemberCard picSrc={picLucas} name="Lucas" subtitle="Consultor" urlLinkedIn="https://www.linkedin.com/in/lchehade/" />
-                <MemberCard picSrc={picMarco} name="Marco" subtitle="Consultor" urlLinkedIn="https://www.linkedin.com/in/marcosartor-/" />
             </Row>
         </InfoCard >
     );
